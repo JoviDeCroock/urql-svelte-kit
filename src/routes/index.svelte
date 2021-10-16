e43b9193441f8b6962b626873ea0eee55bf20012
JoviDeCroock <decroockjovi@gmail.com>
2021-10-16 23:01:29 +0200

    start

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,54 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  export async function load(ctx) {
+		const query = `
+      query ($skip: Int!) {
+        pokemons(limit: 10, skip: $skip) {
+          id
+          name
+        }
+      }
+		`
+		
+		console.log(ctx)
+    return {
+      props: {
+        todos: await ctx.client.query(
+          query,
+          { skip: 0 },
+        ),
+      },
+    }
+  }
 </script>
 
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+<script>
+  import { query } from '@urql/svelte'
+  /**
+   * @type {import('./types').OperationStore<gql.GetTodosDocument, gql.GetTodos['viewer']['todos']>}
+   */
+  export let todos
+  // Setup subscription for the lifetime of the component
+  query(todos)
+  
+  // Data is normalized
+  assert($todos.data.viewer.todos === $todos.data.todos)
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<!-- <div>
+  {#if $pokemons.fetching}
+    Loading...
+  {:else if $pokemons.error}
+    Oh no... {$pokemons.error.message}
+  {:else}
+    <ul>
+      {#each $pokemons.data.pokemons as pokemon}
+        <li>{pokemon.name}</li>
+      {/each}
+    </ul>
+  {/if}
+  <button on:click={nextPage}>Next Page</button>
+</div> -->
